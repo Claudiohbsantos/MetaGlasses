453a56228ced8340196bf22757949a360349d1ac
Claudio Santos <claudiohbsantos@gmail.com>
2020-11-19 14:30:20 -0800

    remove track listing from metadata

--- src/components/MetadataTable.tsx
+++ src/components/MetadataTable.tsx
@@ -79,9 +79,6 @@ class MetadataTable extends React.Component<{ metadata: BWFMetadata }, MetadataT
             {entry('Timecode start', md.timecode)}
             {entry('Userbits', md.userbits)}
           </Row>
-          <Row>
-            {entry('Tracks', md.tracks?.map((t) => `${t.interleaveChannel}:${t.recorderChannel}:${t.name}`).join('\n'))}
-          </Row>
           <Row>{entry('Circled', formCircledValue(md.circled))}</Row>
           <Row>{entry('Notes', md.note)}</Row>
         </Container>
